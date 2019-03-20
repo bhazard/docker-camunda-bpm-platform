2fcf7a3cde170868c61d8ebe8347f9381eb3cc0d
Bill Hazard <bhazard2@gmail.com>
2019-03-20 08:42:48 -0400

    Tidy worker.js

--- example/js/worker.js
+++ example/js/worker.js
@@ -4,7 +4,11 @@ const { Client, logger } = require('camunda-external-task-client-js');
 //  - 'baseUrl': url to the Process Engine
 //  - 'logger': utility to automatically log important events
 //  - 'asyncResponseTimeout': long polling timeout (then a new request will be issued)
-const config = { baseUrl: 'http://localhost:8084/engine-rest', use: logger, asyncResponseTimeout: 10000 };
+const config = { 
+  baseUrl: 'http://localhost:8084/engine-rest', 
+  use: logger, 
+  asyncResponseTimeout: 10000 
+};
 
 // create a Client instance with custom configuration
 const client = new Client(config);
